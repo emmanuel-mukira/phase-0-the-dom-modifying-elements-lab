e112f130e3ccb4b5cd3b6f3e53f5185436dcf2a2
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-03-22 08:50:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const removeMain=document.getElementById('main')
+removeMain.remove(main);
+const newHeader=document.createElement('h1')
+newHeader.id="victory"
+newHeader.textContent="Shigi is the champion"
